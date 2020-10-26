1ac722945fbda4b02cb0f8a4929b83c1007a3e63
Mark Reynolds <Mark@ProfReynolds.com>
2020-10-26 06:04:28 -0500

    recovered unit 6

--- Mastery Project Unit 6/Main_Application/MainForm.Designer.cs
+++ Mastery Project Unit 6/Main_Application/MainForm.Designer.cs
@@ -31,21 +31,28 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.btnExit = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.lbFileOutput = new System.Windows.Forms.ListBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(61, 297);
+            this.btnExit.Location = new System.Drawing.Point(0, 293);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(202, 43);
+            this.btnExit.Size = new System.Drawing.Size(200, 43);
             this.btnExit.TabIndex = 0;
             this.btnExit.Text = "Exit Application";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -54,20 +61,51 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(30, 19);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(198, 214);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(86, 20);
+            this.toolStripMenuItem1.Text = "Place Holder";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem2.Text = "Help";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 339);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 60);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(145, 48);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open CSV File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
@@ -76,29 +114,44 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(175, 428);
-            this.panel1.TabIndex = 9;
-            // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(235, 120);
+            this.panel2.Controls.Add(this.lbFileOutput);
+            this.panel2.Controls.Add(this.txtFileName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(175, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(122, 203);
-            this.panel2.TabIndex = 10;
+            this.panel2.Size = new System.Drawing.Size(609, 339);
+            this.panel2.TabIndex = 4;
+            // 
+            // lbFileOutput
+            // 
+            this.lbFileOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbFileOutput.FormattingEnabled = true;
+            this.lbFileOutput.ItemHeight = 15;
+            this.lbFileOutput.Location = new System.Drawing.Point(0, 23);
+            this.lbFileOutput.Name = "lbFileOutput";
+            this.lbFileOutput.Size = new System.Drawing.Size(609, 316);
+            this.lbFileOutput.TabIndex = 1;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtFileName.Location = new System.Drawing.Point(0, 0);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(609, 23);
+            this.txtFileName.TabIndex = 0;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Controls.Add(this.btnExit);
-            this.panel3.Location = new System.Drawing.Point(410, 30);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(584, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(268, 379);
-            this.panel3.TabIndex = 11;
+            this.panel3.Size = new System.Drawing.Size(200, 339);
+            this.panel3.TabIndex = 5;
             // 
             // MainForm
             // 
@@ -107,7 +160,7 @@
             this.BackColor = System.Drawing.Color.LightBlue;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 361);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -115,11 +168,15 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 400);
             this.Name = "MainForm";
             this.Text = "Master Assignment - Mark Reynolds";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -130,11 +187,16 @@
 
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ListBox lbFileOutput;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- Mastery Project Unit 6/Main_Application/MainForm.cs
+++ Mastery Project Unit 6/Main_Application/MainForm.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using System.IO;
 
 /*
  * Prof Reynolds
@@ -28,26 +27,39 @@ namespace Main_Application
             Close();
         }
 
-        //private void btnOpenCsvFile_Click(object sender, EventArgs e)
-        //{
-        //    // Displays an OpenFileDialog so the user may select a CSV file
-        //    var openFileDialog = new OpenFileDialog
-        //    {
-        //        Filter = @"Comma Separated Values|*.csv|Text File|*.txt",
-        //        Title = @"Select the Hundred Names database"
-        //    };
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            // Diosplays an OpenFileFialog so the user may select a CSV file
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"Comma Separated Values|*.csv|Text File|*.txt",
+                Title = @"Select the HundredNames database"
+            };
 
-        //    // show the dialog
-        //    // if the user clicked OK, open it
-        //    var dialogResult = openFileDialog.ShowDialog();
-        //    if (dialogResult == DialogResult.OK)
-        //    {
-        //        MessageBox.Show(
-        //            openFileDialog.SafeFileName,
-        //            @"You Selected", MessageBoxButtons.OK,
-        //            MessageBoxIcon.Information);
-        //    }
-        //}
+            // show the dialog
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                MessageBox.Show(
+                    openFileDialog.SafeFileName,
+                    @"Yo Selected",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
 
+                txtFileName.Text = openFileDialog.SafeFileName;
+
+                toolStripStatusLabel1.Text = openFileDialog.FileName;
+
+                using (StreamReader sr = File.OpenText(openFileDialog.FileName))
+                {
+                    var oneLineOfText = "";
+                    while ((oneLineOfText = sr.ReadLine()) != null)
+                    {
+                        lbFileOutput.Items.Add(oneLineOfText);
+                    }
+                }
+
+            }
+        }
     }
 }
